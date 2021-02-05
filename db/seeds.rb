1ed21e0e2a59f29cd41042421167e579e9568f70
Jacob Lepler <lepler.jacob@gmail.com>
2021-02-05 15:24:56 -0500

    commit tree e42717d79702d42199b4bd751770ee27b7d18559

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+    Post.create(
+        title: Faker::Lorem.sentence(word_count: 1, supplemental: true, random_words_to_add: 10),
+        description: Faker::Lorem.paragraph)
+end
